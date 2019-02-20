8c84905f5d0e1057d114d74a591d0f011b6bc1f0
rookiefly <rookiefly@163.com>
2019-02-20 23:30:00 +0800

    算法题目

--- src/main/java/com/rookiefly/commons/algorithm/AlgorithmUtil.java
+++ src/main/java/com/rookiefly/commons/algorithm/AlgorithmUtil.java
@@ -1,15 +1,24 @@
 package com.rookiefly.commons.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 最长公共子字符串
  * @create: 2019-02-20
  **/
-public class LCSUtil {
+public class AlgorithmUtil {
 
     public static void main(String[] args) {
         System.out.println(getLCSString("acbcbcef", "abcbced"));
+        System.out.println(getLengthOfLongestSubstring("abcabcbb"));
     }
 
+    /**
+     * 最长公共子字符串
+     * @param s1
+     * @param s2
+     * @return
+     */
     public static String getLCSString(String s1, String s2) {
         char[] s1Array = s1.toCharArray();
         char[] s2Array = s2.toCharArray();
@@ -35,5 +44,32 @@ public class LCSUtil {
         }
 
         return s2.substring(pos - length + 1, pos + 1);
+    }
+
+    /**
+     * 无重复字符的最长子串
+     * @return
+     */
+    public static String getLengthOfLongestSubstring(String s){
+        if (s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int pre = -1;
+        int length = 0;
+        int pos = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            Integer index = map.get(ch);
+            if (index != null) {
+                pre = Math.max(pre, index);
+            }
+            map.put(ch, i);
+            if (length < i - pre) {
+                length = i - pre;
+                pos = i;
+            }
+        }
+        return s.substring(pos + 1 - length, pos + 1);
     }
 }
--- src/main/java/com/rookiefly/commons/algorithm/TreeNode.java
+++ src/main/java/com/rookiefly/commons/algorithm/TreeNode.java
@@ -1,4 +1,14 @@
 package com.rookiefly.commons.algorithm;
 
 public class TreeNode {
+
+    private int value;
+
+    private TreeNode left;
+
+    private TreeNode right;
+
+    public TreeNode(int value){
+        this.value = value;
+    }
 }
