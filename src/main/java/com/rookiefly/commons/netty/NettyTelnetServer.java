93d352ed5049c745acf4500b24eee9ea1601aa79
rookiefly <rookiefly@163.com>
2019-03-26 20:06:18 +0800

    netty telnet server

---
+++ src/main/java/com/rookiefly/commons/netty/NettyTelnetServer.java
@@ -0,0 +1,50 @@
+package com.rookiefly.commons.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Netty实现telnet服务
+ */
+public class NettyTelnetServer {
+
+    private static final int PORT = 8888;
+    private ServerBootstrap serverBootstrap;
+
+    private EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    private EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    public void open() throws InterruptedException {
+
+        serverBootstrap = new ServerBootstrap();
+        serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new NettyTelnetInitializer());
+
+        Channel ch = serverBootstrap.bind(PORT).sync().channel();
+
+        ch.closeFuture().sync();
+    }
+
+    public void close() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        NettyTelnetServer nettyTelnetServer = new NettyTelnetServer();
+        try {
+            nettyTelnetServer.open();
+        } catch (InterruptedException e) {
+            nettyTelnetServer.close();
+        }
+    }
+}
